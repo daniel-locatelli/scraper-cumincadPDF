9a95e5748ba0235da786c3cbd863e880b616c6ac
Daniel Locatelli <daniel.nlocatelli@gmail.com>
2023-02-22 22:20:43 +0100

    Working file

--- scraper-cumincadPDF.py
+++ scraper-cumincadPDF.py
@@ -6,29 +6,37 @@
 # ----------------------------------------------------------------------------------------------------------------------
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import os
-import re
 import time
+import random
 # ----------------------------------------------------------------------------------------------------------------------
 
-Source = "ACADIA" 
+series = "ACADIA" 
 
-folder = Source + "/" 
+folder = series + "/"
 
-def getPaperLinks(main_soup):
+pdfBaseUrl = 'http://papers.cumincad.org/data/works/att/'
+
+def getPaperUrls(main_soup):
     '''find all papers link and return the list of papers'''
     form = main_soup.find('form', attrs={'name': 'main'})
-    children = form.find_all('b')       #gets tag <b>
-    paperLinks = []
-    for child in children:              #gets the parent of tag <b>, which is tag <a>. This removes other unecessary <a> tags
+    children = form.find_all('b')               #gets tag <b>
+    linksList = []
+    for child in children:                      #gets the parent of tag <b>, which is tag <a>. This removes other unecessary <a> tags
         parent = child.parent
-        if parent.get('href') != None:  #if tag <a> has a valid url
-            paperNames.append(parent)       #then add tag <a> to the list
-    return papersNames
+        if parent.get('href') != None:          #if tag <a> has a valid url
+            paperUrl = parent.get('href')
+            paperName = paperUrl.split("/")[-1]
+            pdfUrl = pdfBaseUrl + paperName + '.pdf'
+            linksList.append(pdfUrl)         #then add tag <a> to the list
+    return linksList
 
-def downloadPapers(paperLinks):
-    '''download papers''' 
+def downloadPapers(url):
+    '''download papers'''
+    response = requests.get(url)
+    fileName = url.split("/")[-1]
+    with open(folder + fileName, 'wb') as f:
+        f.write(response.content)
 
 def countPapers(folder):
     '''count the number of currently scraped paper'''
@@ -40,20 +48,22 @@ def main(total=16336):
     count = 0
     while page_number <= total:
         # access series site, e.g. ACADIA
-        series_site = "".format(series, page number)
+        series_site = "http://papers.cumincad.org/cgi-bin/works/BrowseTree?field=series&separator=:&recurse=0&order=AZ&value={}&first={}".format(series, page_number)
         # the first page of archive
-        print(page_number)
-        main_request = requests.get(main_site)
+        print('Page number: ' + page_number)
+        main_request = requests.get(series_site)
         main_soup = BeautifulSoup(main_request.text, 'html.parser')
         # get the papers in the page
-        papers = getPapers(main_soup)
+        papersUrl = getPaperUrls(main_soup)
         # extract paper info
-        for paper in papers:
+        for url in papersUrl:
             start = time.time()
-            getPaperInfo(paper)
+            print (url)
+            downloadPapers(url)
             count += 1
             end = time.time()
             print("{}/16336        {} papers,      {} sec".format(count, countPapers(folder), end - start))
+            time.sleep(random.randint(0, 3))
         page_number += 20
 
 
